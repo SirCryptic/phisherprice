79ac63149f079661e6980ff55291bcdf7019a2e5
SirCryptic <48811414+SirCryptic@users.noreply.github.com>
2020-05-24 22:58:06 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -13,35 +13,47 @@ echo "[✔] Checking directories...";
 if [ -d "/usr/share/doc/phisherprice" ] ;
 then
 echo "[◉] A directory phisherprice was found! Do you want to replace it? [Y/n]:" ; 
-read installop
-if [ $installop == "y" ] ; 
+read selection
+if [ $selection == "y" ] ; 
 then
- rm -R "/usr/share/doc/phisherprice"
+ rm -r "/usr/share/doc/phisherprice"
 else
  exit
 fi
 fi
 
- echo "[✔] Installing ...";
- echo "";
- git clone https://github.com/SirCryptic/phisherprice /usr/share/doc/phisherprice;
- echo "#!/bin/bash 
-  /usr/share/doc/phisherprice/phisherprice" '${1+"$@"}' > phisherprice;
- sudo chmod +x phisherprice.sh;
- sudo cp phisherprice /usr/bin/;
- sudo cp sshscan.py /usr/bin/;
- sudo rm phisherprice;
+echo "[✔] Installing ...";
+echo "";
+git clone https://github.com/SirCryptic/phisherprice /usr/local/sbin/phisherprice;
+echo "#!/bin/bash 
+bash /usr/share/doc/phisherprice/phisherprice.sh" '${1+"$@"}' > phisherprice;
+chmod +x phisherprice.sh;
+cp phisherprice /usr/local/sbin/;
+rm phisherprice;
 
 
 if [ -d "/usr/share/doc/phisherprice" ] ;
 then
-echo "";
-echo "[✔]Script istalled with success![✔]";
-echo "";
-  echo "[✔]====================================================================[✔]";
-  echo "[✔]   You can execute the tool by typing  sudo phisherprice  !         [✔]"; 
-  echo "[✔]====================================================================[✔]";
-  echo "";
+echo -e '\e[1;33m
+__________.__    .__       .__                __________        .__              
+\______   \  |__ |__| _____|  |__   __________\______   \_______|__| ____  ____  
+ |     ___/  |  \|  |/  ___/  |  \_/ __ \_  __ \     ___/\_  __ \  |/ ___\/ __ \ 
+ |    |   |   Y  \  |\___ \|   Y  \  ___/|  | \/    |     |  | \/  \  \__\  ___/ 
+ |____|   |___|  /__/____  >___|  /\___  >__|  |____|     |__|  |__|\___  >___  >
+               \/        \/     \/     \/ PhisherPrice || Version 2.1   \/    \/\e[1;34m
+                                
+                                Created by "Sir Cryptic ~ NULLSec"
+                    NULL Security Forums | https://nullsec.online\e[1;31m
+  Usage of PhisherPrice for attacking targets without prior mutual consent is ILLEGAL. 
+  The Developer is NOT responsible for any damage caused by this script.
+  PhisherPrice is intented FOR EDUCATIONAL PURPOSES ONLY!
+[✔]====================================================================[✔]
+[✔]               phisherprice installed successfully!                 [✔]
+[✔]====================================================================[✔]
+[✔] ✔✔✔  You can execute the script by typing phisherprice   !     ✔✔✔ [✔]
+[✔]====================================================================[✔]
+\e[1;36m
+'
 else
   echo "[✘] Installation failed![✘] ";
   exit
