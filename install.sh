6c1b6e65d52b46a843a70eee605172a627266b8d
SirCryptic <48811414+SirCryptic@users.noreply.github.com>
2020-05-24 22:26:17 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -13,48 +13,35 @@ echo "[✔] Checking directories...";
 if [ -d "/usr/share/doc/phisherprice" ] ;
 then
 echo "[◉] A directory phisherprice was found! Do you want to replace it? [Y/n]:" ; 
-read selection
-if [ $selection == "y" ] ; 
+read installop
+if [ $installop == "y" ] ; 
 then
- rm -r "/usr/share/doc/phisherprice"
+ rm -R "/usr/share/doc/phisherprice"
 else
  exit
 fi
 fi
 
-echo "[✔] Installing ...";
-echo "";
-git clone https://github.com/SirCryptic/phisherprice /usr/share/doc/phisherprice;
-echo "#!/bin/bash 
-bash /usr/share/doc/phisherprice/phisherprice.sh" '${1+"$@"}' > phisherprice;
-chmod +x phisherprice;
-sudo cp phisherprice /usr/bin/;
-sudo cp sshscan.py /usr/bin/
-rm phisherprice;
+ echo "[✔] Installing ...";
+ echo "";
+ git clone https://github.com/SirCryptic/phisherprice /usr/share/doc/phisherprice;
+ echo "#!/bin/bash 
+ bash /usr/share/doc/phisherprice/phisherprice.sh" '${1+"$@"}' > phisherprice;
+ chmod +x phisherprice;
+ sudo cp phisherprice /usr/bin/;
+ sudo cp sshscan /usr/bin/;
+ rm phisherprice;
 
 
 if [ -d "/usr/share/doc/phisherprice" ] ;
 then
-echo -e '\e[1;33m
-__________.__    .__       .__                __________        .__              
-\______   \  |__ |__| _____|  |__   __________\______   \_______|__| ____  ____  
- |     ___/  |  \|  |/  ___/  |  \_/ __ \_  __ \     ___/\_  __ \  |/ ___\/ __ \ 
- |    |   |   Y  \  |\___ \|   Y  \  ___/|  | \/    |     |  | \/  \  \__\  ___/ 
- |____|   |___|  /__/____  >___|  /\___  >__|  |____|     |__|  |__|\___  >___  >
-               \/        \/     \/     \/ PhisherPrice || Version 2.1   \/    \/\e[1;34m
-                                
-                                Created by "Sir Cryptic ~ NULLSec"
-                    NULL Security Forums | https://nullsec.online\e[1;31m
-  Usage of PhisherPrice for attacking targets without prior mutual consent is ILLEGAL. 
-  The Developer is NOT responsible for any damage caused by this script.
-  PhisherPrice is intented FOR EDUCATIONAL PURPOSES ONLY!
-[✔]====================================================================[✔]
-[✔]               phisherprice installed successfully!                 [✔]
-[✔]====================================================================[✔]
-[✔] ✔✔✔  You can execute the script by typing phisherprice   !     ✔✔✔ [✔]
-[✔]====================================================================[✔]
-\e[1;36m
-'
+echo "";
+echo "[✔]Script istalled with success![✔]";
+echo "";
+  echo "[✔]====================================================================[✔]";
+  echo "[✔]   You can execute the tool by typing  sudo phisherprice  !         [✔]"; 
+  echo "[✔]====================================================================[✔]";
+  echo "";
 else
   echo "[✘] Installation failed![✘] ";
   exit
