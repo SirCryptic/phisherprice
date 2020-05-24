00ae0d424a0be61b159076a643df41d19f27c2cb
SirCryptic <48811414+SirCryptic@users.noreply.github.com>
2020-05-24 22:19:36 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ echo "";
 git clone https://github.com/SirCryptic/phisherprice /usr/share/doc/phisherprice;
 echo "#!/bin/bash 
 bash /usr/share/doc/phisherprice/phisherprice.sh" '${1+"$@"}' > phisherprice;
-chmod +x phisherprice.sh;
+chmod +x phisherprice;
 sudo cp phisherprice /usr/bin/;
 sudo cp sshscan.py /usr/bin/
 rm phisherprice;
