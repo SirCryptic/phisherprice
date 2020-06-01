388490c4a8c3db22cd28b4a90cf118fa912e45e5
SirCryptic <48811414+SirCryptic@users.noreply.github.com>
2020-06-01 14:21:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ git clone https://github.com/SirCryptic/phisherprice /usr/share/doc/phisherprice
 echo "#!/bin/bash 
 bash /usr/share/doc/phisherprice/phisherprice.sh" '${1+"$@"}' > phisherprice;
 chmod +x phisherprice.sh;
-sudo cp phisherprice /usr/bin/;
+sudo cp phisherprice.sh /usr/bin/;
 sudo cp sshscan.py /usr/bin/;
 rm phisherprice;
 
