f7720572813bd8338cf10020b17a8929402c0604
sircryptic <48811414+SirCryptic@users.noreply.github.com>
2020-06-30 23:53:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,7 +59,6 @@ sudo python get-pip.py -y
 rm get-pip.py
 pip install psycopg2 -y
 sudo apt install arp-scan
-sudo apt install -y set
 git clone https://github.com/trustedsec/social-engineer-toolkit/ set/
 cd set
 pip install -r requirements.txt
