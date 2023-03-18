57d9a780f64a8715d164c77c72f22a305db2cfc5
SirCryptic <48811414+SirCryptic@users.noreply.github.com>
2023-03-18 05:56:52 +0000

    -

--- phisherprice.sh
+++ phisherprice.sh
@@ -233,7 +233,7 @@ else
     echo -e '**************************************************\n'
 fi
         ;;
-   u|U)
+u|U)
     clear
     echo "${green}Updating Phisherprice...${reset}"
     REPO_URL="https://github.com/sircryptic/phisherprice.git"
@@ -258,16 +258,15 @@ fi
     echo "${yellow}Making $INSTALL_DIR/phisherprice.sh executable...${reset}"
     sudo chmod +x $INSTALL_DIR/phisherprice.sh || { echo "${red}Failed to make phisherprice.sh executable${reset}"; exit 1; }
 
-    echo "${yellow}Copying required files to $INSTALL_DIR...${reset}"
-    sudo cp admin_urls.txt $INSTALL_DIR || { echo "${red}Failed to copy admin_urls.txt${reset}"; exit 1; }
-    sudo cp user_agents.txt $INSTALL_DIR || { echo "${red}Failed to copy user_agents.txt${reset}"; exit 1; }
-
     echo "${yellow}Creating symbolic link for phisherprice.sh as pp...${reset}"
     if [ -L /usr/local/bin/pp ]; then
         echo "${yellow}Removing old symbolic link...${reset}"
         sudo rm /usr/local/bin/pp || { echo "${red}Failed to remove old symbolic link${reset}"; exit 1; }
     fi
     sudo ln -s $INSTALL_DIR/phisherprice.sh /usr/local/bin/pp || { echo "${red}Failed to create symbolic link${reset}"; exit 1; }
+
+    echo "${green}Copying admin_urls.txt and user_agents.txt to $INSTALL_DIR...${reset}"
+    sudo cp admin_urls.txt user_agents.txt $INSTALL_DIR || { echo "${red}Failed to copy files${reset}"; exit 1; }
 
     echo "${green}Update complete!${reset}"
 
